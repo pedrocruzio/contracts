1023e5bcea7d513f83f9fb32289510b7a663e3f3
Krishang Nadgauda <nkrishang@Krishangs-MBP.lan>
2022-06-15 12:22:27 -0400

    TokenGate: token gate NFT acquisition (ERC721/1155)

---
+++ contracts/feature/TokenGate.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+import "./TokenBundle.sol";
+
+abstract contract TokenGate is TokenBundle {
+
+    uint256 private constant DEFAULT_ID = type(uint256).max;
+
+    function setTokenGate(uint256 _id, Token[] calldata _restrictions) public {
+        require(_canSetTokenGate(), "Unauthorized caller");
+        require(_id < DEFAULT_ID, "Setting default token gate");
+        _setBundle(_restrictions, _id);
+    }
+
+    function setDefaultTokenGate(Token[] calldata _restrictions) public {
+        require(_canSetTokenGate(), "Unauthorized caller");
+        _setBundle(_restrictions, DEFAULT_ID);
+    }
+
+    function _canSetTokenGate() internal virtual returns (bool);
+
+    /// @dev See {ERC721-_beforeTokenTransfer}.
+    function _beforeTokenTransfer(
+        address,
+        address to,
+        uint256 tokenId
+    ) internal virtual {
+
+        if(getTokenCountOfBundle(DEFAULT_ID) == 0 && getTokenCountOfBundle(tokenId) == 0) {
+            return;
+        }
+
+        _gate(tokenId, to);
+    }
+
+    /// @dev See {ERC1155-_beforeTokenTransfer}.
+    function _beforeTokenTransfer(
+        address,
+        address,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory,
+        bytes memory
+    ) internal virtual {
+
+        bool defaultIsEmpty = getTokenCountOfBundle(DEFAULT_ID) == 0;
+        bool toCheckDefault;
+
+        for(uint256 i = 0; i < ids.length; i += 1) {
+            if(getTokenCountOfBundle(ids[i]) > 0) {
+                _gate(ids[i], to);
+            } else if (!toCheckDefault && !defaultIsEmpty) {
+                toCheckDefault = true;
+            }
+        }
+
+        if(toCheckDefault) {
+            _gate(DEFAULT_ID, to);
+        }
+    }
+
+    function _gate(uint256 _tokenId, address _target) internal view {
+        uint256 id = getTokenCountOfBundle(_tokenId) == 0 ? DEFAULT_ID : _tokenId;
+        uint256 count = getTokenCountOfBundle(id);
+
+        for(uint256 i = 0; i < count; i += 1) {
+            Token memory token = getTokenOfBundle(id, i);
+            bool success = _checkBalance(token, _target);
+
+            require(success, "TokenGate: insufficient token ownership.");
+        }
+    }
+
+    function _checkBalance(Token memory _token, address _target) internal view returns (bool success) {
+        if(_token.tokenType == TokenType.ERC20) {
+            success = IERC20(_token.assetContract).balanceOf(_target) > _token.totalAmount;
+        } else if (_token.tokenType == TokenType.ERC721) {
+            success = IERC721(_token.assetContract).ownerOf(_token.tokenId) == _target;
+        } else if (_token.tokenType == TokenType.ERC1155) {
+            success = IERC1155(_token.assetContract).balanceOf(_target, _token.tokenId) > _token.totalAmount;
+        }
+    }
+}
